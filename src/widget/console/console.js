0457c2f526334c334edfb59dfc1e71f02a599094
Xiaojue <designsor@gmail.com>
2011-08-19 13:35:50 +0800

    console.js updeta

--- src/widget/console/console.js
+++ src/widget/console/console.js
@@ -4,8 +4,19 @@
  * 全局的console对象，对其扩展之后，拥有自带调试的走自带，没有的，则返回空
  * 避免调试代码污染  方便进行调试的开发模式
  */
-(function(W,G){
+(function(W,G,$){
 	
+	if(!W.console){
+		W.console={}
+		var method=['log','debug','info','warn','exception','assert','dir','dirxml','trace','group','groupEnd','groupCollapsed','time','timeEnd','profile','profileEnd','count','clear','table','error','notifyFirebug','firebug','userObjects'];
+			options=function(){
+				var i,options={};
+				for(i=0;i<method.length;i++){
+					options[method[i]]==$.noop;
+				}
+			}();
+			
+		$.extend(W.console,options);
+	};
 	
-	
-})(window,GM);
+})(window,GM,jQuery);
