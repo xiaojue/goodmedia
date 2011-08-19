bda70872a899d0033c7a6c16038a49720e5d628f
Xiaojue <designsor@gmail.com>
2011-08-19 13:21:34 +0800

    add console and debug js

---
+++ src/widget/console/console.js
@@ -0,0 +1,11 @@
+/**
+ * @author fuqiang [designsor@gmail.com]
+ * @date 20110819
+ * 全局的console对象，对其扩展之后，拥有自带调试的走自带，没有的，则返回空
+ * 避免调试代码污染  方便进行调试的开发模式
+ */
+(function(W,G){
+	
+	
+	
+})(window,GM);
---
+++ src/widget/debug/debug.js
@@ -0,0 +1,9 @@
+/**
+ * @author fuqiang [designsor@gmail.com]
+ * @date 20110819
+ * debug模式下增加debug工具
+ * 应该包含统计，效率，测试等扩展面板
+ */
+(function(W,G){
+	
+})(window,GM);
