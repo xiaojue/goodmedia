90500c394950911deb3043c03660ebac9d5266e4
Xiaojue <designsor@gmail.com>
2011-09-09 13:48:09 +0800

    deleted some swp

--- bundle.rb
+++
@@ -1,7 +0,0 @@
-template "Sample File Template" do |t|
-  t.filetype = "*.txt"
-  t.invoke do |context|
-    raw_contents = IO.read("#{File.dirname(ENV['TM_BUNDLE_SUPPORT'])}/templates/sample.html")
-    raw_contents.gsub(/\$\{([^}]*)\}/) {|match| ENV[match[2..-2]] }
-  end
-end
--- css/test.js
+++
@@ -1,7 +0,0 @@
-function incise(htmlstr,n){
-	
-	var str=htmlstr.replace(/\s/,'');
-		
-		
-		
-}
