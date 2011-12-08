79469c4ac0ee5c6a7fd87ebdb28c2521775ff912
Xiaojue <designsor@gmail.com>
2011-12-08 15:52:12 +0800

    add roll demo and complete but when you changes towards it\'s have a little bug.

--- src/widget/roll/roll.js
+++ src/widget/roll/roll.js
@@ -8,14 +8,15 @@
 		this.wrap = wrap;
 		this.scrollWrap = '#J_ScrollWrap' + new Date().valueOf();
 		this.T = null;
+		this.clone = null;
 		var _config = {
 			towards: 'up',
 			//4个prototype方向
 			height: 300,
 			width: 300,
-			speed: 300,
+			speed: 200,
 			auto: true,
-			endtype: 'endtoend' // endtoend 首位相接 | stop 到头后停止 | back 到头后反向滚
+			endtype: 'endtoend' // endtoend 首位相接
 		};
 		$.extend(_config, config);
 		this.config = _config;
@@ -26,6 +27,7 @@
 			var that = this,
 			wrap = $(that.wrap),
 			scrollWrap = $(that.scrollWrap),
+      clone = that.clone,
 			transverse = function() {
 				return {
 					left: wrap.position().left
@@ -38,59 +40,190 @@
 			},
 			top = wrap.position().top,
 			left = wrap.position().left,
-			clone;
-			that.stop();
-
+      clonetop=clone.position().top,
+      cloneleft=clone.position().left;
 			var count = {
 				up: function() {
 					top--;
+          clonetop--;
 					wrap.css('top', top);
+					that.clone.css('top', clonetop);
 				},
 				down: function() {
 					top++;
+          clonetop++;
 					wrap.css('top', top);
+				  that.clone.css('top', clonetop);
 				},
 				left: function() {
 					left--;
+          cloneleft--;
 					wrap.css('left', left);
+					that.clone.css('left', cloneleft);
 				},
 				right: function() {
 					left++;
+          cloneleft++;
 					wrap.css('left', left);
+					that.clone.css('left', cloneleft);
 				}
 			};
 
+			var endhandle = {
+				endtoend: function() {
+					count[towards](); //一直滚，把当前的滚完，偏移设置回0
+					var verticalhandle = function() {
+            if (Math.abs(top) >= wrap.height() || Math.abs(clonetop) >= wrap.height()) {
+							wrap.css('top', 0);
+							if (towards === "up") that.clone.css('top', wrap.height());
+							if (towards === "down") that.clone.css('top', - wrap.height());
+						}
+					};
+					var transversehandle = function() {
+            if (Math.abs(left) >= wrap.width() || Math.abs(cloneleft) >= wrap.width()) {
+							wrap.css('left', 0);
+							if (towards === "left") that.clone.css('left', wrap.width());
+							if (towards === "right") that.clone.css('left', - wrap.width());
+						}
+					};
+
+					switch (towards) {
+					case "up":
+						verticalhandle();
+						break;
+					case "down":
+						verticalhandle();
+						break;
+					case "left":
+						transversehandle();
+						break;
+					case "right":
+						transversehandle();
+						break;
+					default:
+						break;
+					}
+				}
+			};
+			var handle = {
+				up: function() {
+					if (Math.abs(top) <= wrap.height() - scrollWrap.height()) {
+						count[towards]();
+					} else {
+						endhandle[that.config.endtype]();
+					}
+				},
+				down: function() {
+					if (Math.abs(top) < scrollWrap.height()) {
+						count[towards]();
+					} else {
+						endhandle[that.config.endtype]();
+					}
+				},
+				left: function() {
+					if (Math.abs(left) <= wrap.width() - scrollWrap.width()) {
+						count[towards]();
+					} else {
+						endhandle[that.config.endtype]();
+					}
+				},
+				right: function() {
+					if (Math.abs(left) < scrollWrap.width()) {
+						count[towards]();
+					} else {
+						endhandle[that.config.endtype]();
+					}
+				}
+			};
+
+			handle[towards]();
+
+		},
+		changeTowards: function(towards) {
+			this.config.towards = towards;
+			this.stop();
+			this.run();
+		},
+		changespeed: function(speed) {
+			this.config.speed = speed;
+			this.stop();
+			this.run();
+		},
+		auto: function(auto) {
+			this.config.auto = auto;
+			this.stop();
+			this.run();
+		},
+		run: function() {
+			var that = this,
+			wrap = $(that.wrap),
+			cg = that.config,
+			towards = cg.towards,
+			scrollWrap = $(that.scrollWrap);
+
+			var ruler = {
+				transverse: (towards === "left" || towards === "right") ? (scrollWrap.width() < wrap.width()) : true,
+				vertical: (towards === "up" || towards === "down") ? (scrollWrap.height() < wrap.height()) : true
+			};
+
+			if (ruler['transverse'] && ruler['vertical'] && that.config.auto) {
+				that.T = setInterval(function() {
+					that._roll(towards);
+				},
+				that.config.speed);
+			}
+		},
+		stop: function() {
+			var that = this;
+			clearInterval(that.T);
+		},
+		init: function() {
+			var that = this,
+			cg = that.config,
+			wrap = $(that.wrap);
+			wrap.wrap("<div id='" + that.scrollWrap.slice(1) + "'></div>").css({
+				position: 'absolute',
+				top: 0,
+				left: 0
+			});
+			$(that.scrollWrap).css({
+				width: cg.width,
+				height: cg.height,
+				overflow: 'hidden',
+				position: 'relative'
+			});
 			//如果是endtoend的话，提前建立clone节点
 			if (that.config.endtype === "endtoend") {
-				clone = wrap.clone();
-				switch (towards) {
+				that.clone = wrap.clone();
+				that.clone.attr('id', 'J_' + new Date().valueOf());
+				switch (cg.towards) {
 				case "up":
-					wrap.next(clone);
-					clone.css({
+					wrap.parent().append(that.clone);
+					that.clone.css({
 						left:
 						0,
 						top: wrap.height()
 					});
 					break;
 				case "down":
-					wrap.prev(clone);
-					clone.css({
+					wrap.parent().prepend(that.clone);
+					that.clone.css({
 						left:
 						0,
 						top: - wrap.height()
 					});
 					break;
 				case "left":
-					wrap.next(clone);
-					clone.css({
+					wrap.parent().append(that.clone);
+					that.clone.css({
 						top:
 						0,
 						left: wrap.width()
 					});
 					break;
 				case "right":
-					wrap.prev(clone);
-					clone.css({
+					wrap.parent().prepend(that.clone);
+					that.clone.css({
 						top:
 						0,
 						left: - wrap.width()
@@ -100,158 +233,6 @@
 					break;
 				}
 			}
-
-			var endhandle = {
-				endtoend: function(towards) {
-					count[towards](); //一直滚，把当前的滚完，偏移设置回0
-					var verticalhandle = function() {
-						if (Math.abs(top) >= wrap.height()) {
-							wrap.css('top', 0);
-							if (towards === "up") clone.css('top', wrap.height());
-							if (towards === "down") clone.css('top', - wrap.height());
-						}
-					};
-					var transversehandle = function() {
-						if (Math.abs(left) >= wrap.width()) {
-							wrap.css('left', 0);
-							if (towards === "left") clone.css('left', wrap.width());
-							if (towards === "right") clone.css('left', - wrap.width());
-						}
-					};
-					switch (towards) {
-					case "up":
-						verticalhandle();
-						break;
-					case "down":
-						verticalhandle();
-						break;
-					case "left":
-						transversehandle();
-						break;
-					case "right":
-						transversehandle();
-						break;
-					default:
-						break;
-					}
-				},
-				stop: function() {
-					//清除定时器，停止滚动
-					that.stop();
-				},
-				back: function(towards) {
-					//反向滚动
-					switch (towards) {
-					case "up":
-						count["down"]();
-						that.config.towards = "down";
-						break;
-					case "down":
-						count["up"]();
-						that.config.towards = "up";
-						break;
-					case "left":
-						count["right"]();
-						that.config.towards = "right";
-						break;
-					case "right":
-						count["left"]();
-						that.config.towards = "left";
-						break;
-					default:
-						break;
-					}
-				}
-			};
-			var handle = {
-				up: function() {
-					if (Math.abs(top) < wrap.height() - scrollWrap.height()) {
-						count[towards]();
-					} else {
-						endhandle[that.config.endtype]();
-					}
-					$.event.trigger('roll:up', [vertical()]);
-				},
-				down: function() {
-					if (Math.abs(top) < wrap.height() - scrollWrap.height()) {
-						count[towards]();
-					} else {
-						endhandle[that.config.endtype]();
-					}
-					$.event.trigger('roll:down', [vertical()]);
-				},
-				left: function() {
-					if (Math.abs(left) < wrap.width() - scrollWrap.width()) {
-						count[towards]();
-					} else {
-						endhandle[that.config.endtype]();
-					}
-					$.event.trigger('roll:left', [transverse()]);
-				},
-				right: function() {
-					if (Math.abs(top) < wrap.width() - scrollWrap.width()) {
-						count[towards]();
-					} else {
-						endhandle[that.config.endtype]();
-					}
-					$.event.trigger('roll:right', [transverse()]);
-				}
-			};
-
-			handle[towards]();
-
-		},
-    chaneTowards:function(towards){
-      this.config.towards=towards;
-    },
-    //加速
-    upshift:function(){
-      this.recoverspeed=this.config.speed;
-      this.config.speed=this.config.speed/2;
-
-    },
-    //还原速度
-    recoverspeed:function(){
-      this.config.speed=this.recoverspeed;
-    },
-		run: function() {
-			var that = this,
-			wrap = $(that.wrap),
-			cg = that.config,
-      towards=cg.towards,
-			scrollWrap = $(that.scrollWrap);
-
-			var ruler = {
-				transverse: (towards === "left" || towards === "right") ? (scrollWrap.width() < wrap.width()) : true,
-				vertical: (towards === "up" || towards === "down") ? (scrollWrap.height() < wrap.height()) : true
-			};
-
-			if (ruler['transverse'] && ruler['vertical']) {
-				that.T = setInterval(function() {
-					that._roll(towards);
-				},
-				that.config.speed);
-			}
-		},
-		stop: function() {
-			clearInterval(that.T);
-		},
-		init: function() {
-			var that = this,
-			cg = that.config,
-			wrap = that.wrap;
-			$(warp).wrap("<div id='" + that.scrollWrap.slice(1) + "'></div>");
-			$(that.scrollWrap).css({
-				width: cg.width,
-				height: cg.height,
-				overflow: 'hidden',
-				position: 'relative'
-			});
-			$(wrap).css({
-				position: 'absolute',
-				top: 0,
-				left: 0
-			});
 			that.run();
 		}
 	};
